9aa477aea5452ab054ff32bef96277afbf5e2a77
Antonio Furtado <antoniordf@hotmail.com>
2023-04-03 23:36:25 +0100

    Wrote a few more tests

--- test/starNotary.js
+++ test/starNotary.js
@@ -13,3 +13,21 @@ it("has correct name", async () => {
   let starName = await instance.starName.call();
   assert.equal(starName, "Gita");
 });
+
+it("can be claimed", async () => {
+  let instance = await StarNotary.deployed();
+  await instance.claimStar({ from: owner });
+  let starOwner = await instance.starOwner.call();
+  assert.equal(starOwner, owner);
+});
+
+it("can change owners", async () => {
+  let instance = await StarNotary.deployed();
+  let secondUser = accounts[1];
+  await instance.claimStar({ from: owner });
+  let starOwner = await instance.starOwner.call();
+  assert.equal(starOwner, owner);
+  await instance.claimStar({ from: secondUser });
+  let secondOwner = await instance.starOwner.call();
+  assert.equal(secondOwner, secondUser);
+});
